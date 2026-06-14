0d91516bbf65f508c4e14103093befd3020c46d3
pavitra-sudo <213853274+pavitra-sudo@users.noreply.github.com>
2026-06-14 15:33:16 +0530

    Add database connection logic to main application

--- api/main.py
+++ api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from config.settings import conf
 from v1.product.router import router as product_router
+from database.db import engine
+
+try:
+    engine.connect()
+    print(".............................Database connection successful!....................................")
+except Exception as e:
+    print(f"Database connection failed: {e}")
+
+
+
 app = FastAPI()
+
+
 
 app.include_router(product_router)
 
